12f2daaf8d31c45caafd178dfb323771cf2eb105
ExcitedQe <213161557@seu.edu.cn>
2018-12-12 21:04:57 +0800

    edit banner

--- banner.ps1
+++ banner.ps1
@@ -8,26 +8,26 @@ IF($username -eq '36064'){
 $freemem=(get-wmiobject -class Win32_PerfFormattedData_PerfOS_Memory   -namespace "root\cimv2").AvailableMBytes
 $mem =(((Get-WmiObject -Class Win32_PhysicalMemory).capacity | Measure-Object -Sum).sum /1mb)
 
-$outobject = New-Object object
-$outobject | Add-Member NoteProperty TODAY "$($time.Year)-$($time.Month)-$($time.Day), $($time.DayOfWeek)"
-$outobject | Add-Member NoteProperty TIME "$($time.Hour):$($time.Minute)"
-$outobject | Add-Member NoteProperty PID $PID
-$outobject | Add-Member NoteProperty CULTRUE $PSUICulture
-$outobject | Add-Member NoteProperty MEM "$freemem mb/$mem mb"
-
-"WELLCOME $username! "
-
-$outobject | Format-List *
-
 @"
-===========================================================
-       _               _    _   _                   _    _ 
-      | |             | |  (_) | |                 | |  (_)
-   ___| |__   ___  ___| | ___  | |__  _ __ ___  ___| | ___ 
-  / __| '_ \ / _ \/ _ \ |/ / | | '_ \| '__/ _ \/ _ \ |/ / |
- | (__| | | |  __/  __/   <| | | |_) | | |  __/  __/   <| |
-  \___|_| |_|\___|\___|_|\_\_| |_.__/|_|  \___|\___|_|\_\_|
+=================================================
+|`tTODAY`t==>`t{0}`t|
+|`tTIME`t==>`t{1}`t`t`t|
+|`tMEM`t==>`t{2}`t`t|
+|`tSHELL`t==>`tPowerShell`t`t|
+|`tUSER`t==>`t{3}`t`t`t|
+=================================================
+"@ -f ("$($time.Year)-$($time.Month)-$($time.Day), $($time.DayOfWeek)","$($time.Hour):$($time.Minute)","$freemem mb/$mem mb",$username) | Write-Host -ForegroundColor Blue
 
-===========================================================
-`n
-"@ | Write-Host -ForegroundColor Yellow
+
+# @"
+# ===========================================================
+#        _               _    _   _                   _    _ 
+#       | |             | |  (_) | |                 | |  (_)
+#    ___| |__   ___  ___| | ___  | |__  _ __ ___  ___| | ___ 
+#   / __| '_ \ / _ \/ _ \ |/ / | | '_ \| '__/ _ \/ _ \ |/ / |
+#  | (__| | | |  __/  __/   <| | | |_) | | |  __/  __/   <| |
+#   \___|_| |_|\___|\___|_|\_\_| |_.__/|_|  \___|\___|_|\_\_|
+
+# ===========================================================
+# `n
+# "@ | Write-Host -ForegroundColor Yellow
